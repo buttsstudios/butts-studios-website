792c1ef80a38affec5f23edd402a54d107ce2201
latarence <latarencebutts@yahoo.com>
2021-03-21 23:11:47 -0500

    switching to php email

--- email.php
+++ email.php
@@ -119,7 +119,7 @@ echo $message;
 
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers.= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers.= 'From: <team@runwae.com>' . "\r\n";
+$headers.= 'From: <team@buttsstudios.com>' . "\r\n";
 
 $send_email = mail($to,$subject,$message,$headers);
 
